31a572a9e0b3769cfd56df619be5c6229307ddf9
wxsong <wxxsong@gmail.com>
2020-06-14 13:33:15 +0800

    11 双指针

--- ex11.js
+++ ex11.js
@@ -1,7 +1,9 @@
+//  11. 盛最多水的容器
+
 /**
- * 11. 盛最多水的容器
  * @param {number[]} height
  * @return {number}
+ * 复杂度 n^2
  */
 var maxArea = function (height) {
 	const length = height.length;
@@ -12,6 +14,24 @@ var maxArea = function (height) {
 			const an = height[j];
 			const s = Math.min(am, an) * (j - i);
 			max = Math.max(s, max);
+		}
+	}
+	return max;
+};
+
+// 复杂度 n
+var maxArea = function (height) {
+	const length = height.length;
+	let max = 0;
+	let i = 0;
+	let j = length - 1;
+	while (i < j) {
+		const s = Math.min(height[i], height[j]) * (j - i);
+		max = Math.max(s, max);
+		if (height[i] > height[j]) {
+			j--;
+		} else {
+			i++;
 		}
 	}
 	return max;
